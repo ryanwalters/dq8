493365120d065ecfeb12a029e64eff5a2f3dd7a6
Ryan Walters <rwalters@eb.com>
2015-01-26 14:18:08 -0600

    baby steps...

--- server.js
+++ server.js
@@ -162,19 +162,57 @@ Server.route({
     method: 'GET',
     path: '/1/character/{heroName}',
     handler: function (request, reply) {
-        var query = 'SELECT character.character_id, \
-                        character.character_name, \
-                        character.character_image, \
-                        json_agg(character_ability_type.*) AS ability_types \
-                    FROM character \
+        var query = 'SELECT \
+                        c.character_id, \
+                        c.character_name, \
+                        c.character_image, \
+                        json_agg(ability_row) AS ability_types \
+                    FROM character c \
                     JOIN character_ability_type USING (character_id) \
+                    JOIN ( \
+                        SELECT \
+                            ability_type.* \
+                        FROM ability_type \
+                    ) ability_row ON (ability_row.ability_type_id = character_ability_type.ability_type_id) \
                     WHERE character_id = ' + request.params.heroName + ' \
                     GROUP BY \
-                        character.character_id';
+                        c.character_id';
+
+        var sample = 'SELECT \
+                        d.id, \
+                        d.name, \
+                        json_agg(c_row) AS components \
+                    FROM devices d \
+                    INNER JOIN device_components dc ON (dc.deviceid = d.id) \
+                    INNER JOIN ( \
+                        SELECT \
+                            c.id, \
+                            c.name, \
+                            json_agg(m) AS manufacturers \
+                        FROM components c \
+                        INNER JOIN component_manufacturers cm ON (cm.componentid = c.id) \
+                        INNER JOIN manufacturers m ON (cm.manufacturerid = m.id) \
+                        GROUP BY c.id \
+                    ) c_row ON (c_row.id = dc.componentid) \
+                    GROUP BY d.id';
 
         _query(query, reply);
     }
 });
+
+/*
+
+
+ JOIN ( \
+ SELECT \
+ ability_type.ability_type_id, \
+ ability_type.ability_name, \
+ json_agg(character_ability.*) AS abilities \
+ FROM ability_type \
+ JOIN character_ability USING (ability_id) \
+ GROUP BY ability_type.ability_type_id \
+ ) ability_types USING (ability_type_id) \
+ */
 
 var _query = function (query, reply) {
     PG.connect(process.env.DATABASE_URL, function (error, client, done) {
